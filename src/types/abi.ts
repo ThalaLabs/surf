7cfd3be789d2679ef9eef03e591efc971153b3e2
network <net@d2.plus>
2024-07-12 11:26:07 +0700

    private entry randomness

--- src/types/abi.ts
+++ src/types/abi.ts
@@ -12,7 +12,7 @@ export interface ABIRoot {
 
 export interface ABIFunction {
   name: string;
-  visibility: 'friend' | 'public';
+  visibility: 'friend' | 'public' | 'private';
   is_entry: boolean;
   is_view: boolean;
   generic_type_params: readonly ABIFunctionGenericTypeParam[];
