d65b63b6f93e476bd2c270b8ba6c89114a94f863
SamuelQZQ <qianzheqi@foxmail.com>
2024-08-08 19:22:36 +0800

    support 0x1::object::Object and 0x1::option::Option for view function returns

--- src/types/convertor/returnConvertor.ts
+++ src/types/convertor/returnConvertor.ts
@@ -4,7 +4,9 @@
  */
 
 import { UnknownStruct } from '../common.js';
+import { DefaultABITable } from '../defaultABITable.js';
 import { MoveNonStructTypes, MovePrimitive } from '../moveTypes.js';
+import { ConvertStructFieldOptionType } from './structConvertor.js';
 
 /**
  * Convert an array of return types.
@@ -30,26 +32,30 @@ type ConvertPrimitiveReturnType<TMoveType extends MovePrimitive> =
   TMoveType extends 'bool'
     ? boolean
     : TMoveType extends 'u8'
-    ? number
-    : TMoveType extends 'u16'
-    ? number
-    : TMoveType extends 'u32'
-    ? number
-    : TMoveType extends 'u64'
-    ? string
-    : TMoveType extends 'u128'
-    ? string
-    : TMoveType extends 'u256'
-    ? string
-    : TMoveType extends 'address'
-    ? `0x${string}`
-    : TMoveType extends '0x1::string::String'
-    ? string
-    : never;
+      ? number
+      : TMoveType extends 'u16'
+        ? number
+        : TMoveType extends 'u32'
+          ? number
+          : TMoveType extends 'u64'
+            ? string
+            : TMoveType extends 'u128'
+              ? string
+              : TMoveType extends 'u256'
+                ? string
+                : TMoveType extends 'address'
+                  ? `0x${string}`
+                  : TMoveType extends '0x1::string::String'
+                    ? string
+                    : never;
 
 type ConvertNonStructReturnType<TMoveType extends MoveNonStructTypes> =
   TMoveType extends MovePrimitive
     ? ConvertPrimitiveReturnType<TMoveType>
     : TMoveType extends `vector<${infer TInner}>`
-    ? ConvertReturnType<TInner>[]
-    : UnknownStruct<TMoveType>;
+      ? ConvertReturnType<TInner>[]
+      : TMoveType extends `0x1::object::Object<${string}>`
+        ? { inner: `0x${string}` }
+        : TMoveType extends `0x1::option::Option<${infer TInner}>`
+          ? ConvertStructFieldOptionType<DefaultABITable, TInner>
+          : UnknownStruct<TMoveType>;
