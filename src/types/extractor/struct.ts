0036d0a784d868a9d41b10873150b1c302df0302
21Chani <dmendoncacosta03@gmail.com>
2025-04-01 06:19:01 -0300

    feat: convert string struct to object

---
+++ src/types/extractor/struct.ts
@@ -0,0 +1,35 @@
+import { ABIRoot, ABIStruct } from '../abi.js';
+import { MovePrimitivesMap } from '../moveTypes.js';
+
+// =====================================================
+// DEFINED TYPES
+// =====================================================
+
+// ====================================================
+// STRUCTS
+// ====================================================
+
+export type ExtractStructName<
+  ABI extends ABIRoot,
+  FunctionName extends string,
+> =
+  | `0x1::object::Object<${ABI['address']}::${ABI['name']}::${FunctionName}>`
+  | `${ABI['address']}::${ABI['name']}::${FunctionName}`;
+
+// Extracts a raw Struct from the ABI structs array by its name.
+export type ExtractStruct<
+  Abi extends ABIRoot,
+  T extends Abi['structs'][number]['name'],
+> = Extract<Abi['structs'][number], { name: T }>;
+
+// Convert a raw Struct from abi to an object with its fields and parsed object types
+export type ConvertedStruct<ABI extends ABIRoot, T extends ABIStruct> = {
+  [K in T['fields'][number] as K['name']]: K['type'] extends keyof MovePrimitivesMap
+    ? MovePrimitivesMap[K['type']] // Non Struct Types
+    : K['type'] extends ExtractStructName<ABI, infer StructName>
+      ? StructName extends ABI['structs'][number]['name']
+        ? // Transformed 0x1::object::Object<0x1::coin::CustomStruct> to object.
+          ConvertedStruct<ABI, ExtractStruct<ABI, StructName>>
+        : never
+      : never;
+};
