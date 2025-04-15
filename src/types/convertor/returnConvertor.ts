e2f70100c9331791a875c184884585e44cfb474a
Samuel <qianzheqi@foxmail.com>
2025-04-15 15:52:23 +0800

    Revert "chore: better readable types (#240)" (#244)
    This reverts commit 5804b2f7f44f5a5f8c8419f2e347b22ffde27338.

--- src/types/convertor/returnConvertor.ts
+++ src/types/convertor/returnConvertor.ts
@@ -5,12 +5,7 @@
 
 import { UnknownStruct } from '../common.js';
 import { DefaultABITable } from '../defaultABITable.js';
-import {
-  MoveObject,
-  MoveOption,
-  MovePrimitive,
-  MovePrimitiveMap,
-} from '../moveTypes.js';
+import { MoveNonStructTypes, MovePrimitive } from '../moveTypes.js';
 import { ConvertStructFieldOptionType } from './structConvertor.js';
 
 /**
@@ -27,10 +22,40 @@ export type ConvertReturns<T extends readonly string[]> = T extends readonly [
  * Internal
  */
 type ConvertReturnType<TMoveType extends string> =
+  TMoveType extends MoveNonStructTypes
+    ? // it's a non-struct type
+      ConvertNonStructReturnType<TMoveType>
+    : // it's a struct type
+      UnknownStruct<TMoveType>;
+
+type ConvertPrimitiveReturnType<TMoveType extends MovePrimitive> =
+  TMoveType extends 'bool'
+    ? boolean
+    : TMoveType extends 'u8'
+      ? number
+      : TMoveType extends 'u16'
+        ? number
+        : TMoveType extends 'u32'
+          ? number
+          : TMoveType extends 'u64'
+            ? string
+            : TMoveType extends 'u128'
+              ? string
+              : TMoveType extends 'u256'
+                ? string
+                : TMoveType extends 'address'
+                  ? `0x${string}`
+                  : TMoveType extends '0x1::string::String'
+                    ? string
+                    : never;
+
+type ConvertNonStructReturnType<TMoveType extends MoveNonStructTypes> =
   TMoveType extends MovePrimitive
-    ? MovePrimitiveMap<string>[TMoveType]
-    : TMoveType extends MoveObject
-      ? { inner: `0x${string}` }
-      : TMoveType extends MoveOption<infer TInner>
-        ? ConvertStructFieldOptionType<DefaultABITable, TInner>
-        : UnknownStruct<TMoveType>;
+    ? ConvertPrimitiveReturnType<TMoveType>
+    : TMoveType extends `vector<${infer TInner}>`
+      ? ConvertReturnType<TInner>[]
+      : TMoveType extends `0x1::object::Object<${string}>`
+        ? { inner: `0x${string}` }
+        : TMoveType extends `0x1::option::Option<${infer TInner}>`
+          ? ConvertStructFieldOptionType<DefaultABITable, TInner>
+          : UnknownStruct<TMoveType>;
--- src/types/moveTypes.ts
+++ src/types/moveTypes.ts
@@ -2,51 +2,25 @@
  * Types from Move language
  */
 
-import { AnyNumber } from './common.js';
-
 export type MoveNonStructTypes =
   | MovePrimitive
   | MoveVector
   | MoveObject
   | MoveOption;
 
-/**
- * All primitive simple types that are not complex, such as vector, object or struct
- *
- * @type {HighValue} - The type of high value number, in some cases can return just as string, or as AnyNumber, default as string
- * @returns - A map of Move primitive types to their corresponding TypeScript types
- */
-export type MovePrimitiveMap<HighValue extends AnyNumber | string = string> = {
-  bool: boolean;
+export type MovePrimitive =
+  | 'bool'
+  | 'u8'
+  | 'u16'
+  | 'u32'
+  | 'u64'
+  | 'u128'
+  | 'u256'
+  | 'address'
+  | '0x1::string::String';
 
-  address: `0x${string}`;
-  '0x1::string::String': string;
+export type MoveVector = `vector<${string}>`;
 
-  // Number
-  u8: number;
-  u16: number;
-  u32: number;
-  u64: HighValue;
-  u128: HighValue;
-  u256: HighValue;
+export type MoveObject = `0x1::object::Object<${string}>`;
 
-  'vector<bool>': boolean[];
-  'vector<u8>': string | number[] | Uint8Array;
-  'vector<u16>': number[];
-  'vector<u32>': number[];
-
-  'vector<u64>': HighValue[];
-  'vector<u128>': HighValue[];
-  'vector<u256>': HighValue[];
-  'vector<address>': `0x${string}`[];
-  'vector<string>': string[];
-  'vector<0x1::string::String>': string[];
-};
-
-export type MovePrimitive = keyof MovePrimitiveMap<AnyNumber | string>;
-
-export type MoveVector<I extends string = string> = `vector<${I}>`;
-
-export type MoveObject<I extends string = string> = `0x1::object::Object<${I}>`;
-
-export type MoveOption<I extends string = string> = `0x1::option::Option<${I}>`;
+export type MoveOption = `0x1::option::Option<${string}>`;
