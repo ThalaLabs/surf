7be743fda6511b44c85cb4d815c8986f8cd7a9ac
Chani <62969153+21Chani@users.noreply.github.com>
2025-04-21 03:54:02 -0300

    Chore/Better Types (#246)
    * chore: better readable types
    
    * fix: add support for vector in return convertor back
    
    * fix: matrix types

--- src/types/convertor/argsConvertor.ts
+++ src/types/convertor/argsConvertor.ts
@@ -2,9 +2,14 @@
  * Convert Move type to TypeScript type,
  * for input arguments of view function or entry function.
  */
-
 import { AnyNumber, UnknownStruct } from '../common.js';
-import { MoveNonStructTypes, MovePrimitive } from '../moveTypes.js';
+import {
+  MoveObject,
+  MoveOption,
+  MovePrimitive,
+  MovePrimitiveMap,
+  MoveVector,
+} from '../moveTypes.js';
 
 /**
  * Convert an array of input arguments type.
@@ -16,46 +21,13 @@ export type ConvertArgs<T extends readonly string[]> = T extends readonly [
   ? [ConvertArgType<TArg>, ...ConvertArgs<TRest>]
   : [];
 
-/**
- * Internal
- */
-type ConvertArgType<TMoveType extends string> =
-  TMoveType extends MoveNonStructTypes
-    ? // it's a non-struct type
-      ConvertNonStructArgType<TMoveType>
-    : // it's a struct type
-      UnknownStruct<TMoveType>;
-
-type ConvertPrimitiveArgType<TMoveType extends MovePrimitive> =
-  TMoveType extends 'bool'
-    ? boolean
-    : TMoveType extends 'u8'
-      ? number
-      : TMoveType extends 'u16'
-        ? number
-        : TMoveType extends 'u32'
-          ? number
-          : TMoveType extends 'u64'
-            ? AnyNumber
-            : TMoveType extends 'u128'
-              ? AnyNumber
-              : TMoveType extends 'u256'
-                ? AnyNumber
-                : TMoveType extends 'address'
-                  ? `0x${string}`
-                  : TMoveType extends '0x1::string::String'
-                    ? string
-                    : never;
-
-type ConvertNonStructArgType<TMoveType extends MoveNonStructTypes> =
-  TMoveType extends MovePrimitive
-    ? ConvertPrimitiveArgType<TMoveType>
-    : TMoveType extends `vector<u8>`
-      ? string | number[] | Uint8Array
-      : TMoveType extends `vector<${infer TInner}>`
-        ? ConvertArgType<TInner>[]
-        : TMoveType extends `0x1::object::Object<${string}>`
-          ? `0x${string}`
-          : TMoveType extends `0x1::option::Option<${infer TInner}>`
-            ? ConvertArgType<TInner> | undefined
-            : UnknownStruct<TMoveType>;
+// Internal
+type ConvertArgType<TMoveType extends string> = TMoveType extends MovePrimitive
+  ? MovePrimitiveMap<AnyNumber>[TMoveType]
+  : TMoveType extends MoveObject
+    ? `0x${string}`
+    : TMoveType extends MoveVector<infer TInner>
+      ? ConvertArgType<TInner>[]
+      : TMoveType extends MoveOption<infer TInner>
+        ? ConvertArgType<TInner> | undefined
+        : UnknownStruct<TMoveType>;
