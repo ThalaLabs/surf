35091d1cc20d2a81832ad7f76f625b0c50af3106
SamuelQZQ <qianzheqi@foxmail.com>
2025-05-09 13:48:54 +0800

    fix build

--- example/app/layout.tsx
+++ example/app/layout.tsx
@@ -1,12 +1,9 @@
 'use client';
-import { PetraWallet } from 'petra-plugin-wallet-adapter';
 import './globals.css';
 import { Inter } from 'next/font/google';
 import { AptosWalletAdapterProvider } from '@aptos-labs/wallet-adapter-react';
 
 const inter = Inter({ subsets: ['latin'] });
-
-const wallets = [new PetraWallet()];
 
 export default function RootLayout({
   children,
@@ -14,7 +11,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <AptosWalletAdapterProvider autoConnect plugins={wallets}>
+    <AptosWalletAdapterProvider autoConnect>
       <html lang="en">
         <body className={inter.className}>{children}</body>
       </html>
